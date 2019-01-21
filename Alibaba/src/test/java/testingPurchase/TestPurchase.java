d89c1d5296d9599148834a6cb079c428bf701fd2
mkhan1231 <mahmud8118@gmail.com>
2019-01-21 10:04:56 -0500

    Topics covered

--- Alibaba/src/test/java/testingPurchase/TestPurchase.java
+++ Alibaba/src/test/java/testingPurchase/TestPurchase.java
@@ -7,22 +7,15 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import purchase.Purchase;
 import reporting.TestLogger;
-
 import java.io.IOException;
 
-import static base.CommonAPI.convertToString;
-
 public class TestPurchase extends CommonAPI {
-
-
     Purchase objPurchase;
-
 
     @BeforeMethod
     public void initializationOfElements() {
         objPurchase = PageFactory.initElements(driver, Purchase.class);
     }
-
     @Test
     public void searchItems() throws IOException {
         TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
@@ -31,5 +24,4 @@ public class TestPurchase extends CommonAPI {
         String actual=objPurchase.getSuccessfulOrderMessage();
         Assert.assertEquals(actual,expected);
     }
-
 }
